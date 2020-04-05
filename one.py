14496350e013efc21e7dcae4ce0d1896cde298b6
pragadesh.k <pragadeshk31@gmail.com>
2020-04-05 17:43:29 +0530

    power

---
+++ one.py
@@ -0,0 +1,13 @@
+def onee():
+
+	print('This is one.plhjgljhgljhy')                                 #
+                                                            #
+if __name__ == '__main__':									#
+															#
+	print('Running in the one.py')	
+	onee()						#------------>>>> oruvela one.py inoru two.py la import panna patta ithula irukura
+															#                  elame antha two.py call panum pothu thaan varum..one.py call pana yethum varathu coz
+else:														#                  power(__name__ == '__main__') two.py ku transfer aagiduchhu one.py ah import panunathunaala
+															#
+	print('power has been imported to anothe .pyscript')	#
+    
